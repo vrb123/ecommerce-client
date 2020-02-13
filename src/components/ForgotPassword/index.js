fcd3c26b68c2ae009cd8cd1d928502d40f11f3f7
vitalii <vitalii.vorobij@gmail.com>
2020-02-13 17:37:31 +0200

    Connected ForgotPassword component to Redux

--- src/components/ForgotPassword/index.js
+++ src/components/ForgotPassword/index.js
@@ -2,16 +2,31 @@ import React from 'react';
 import {Row,Col} from 'antd';
 import ForgotPasswordForm from './ForgotPasswordForm';
 
-export default () => {
+import {connect} from 'react-redux';
+import {forgotPassword} from '../../actions/user'
+
+import {Redirect} from 'react-router-dom'
+
+const ForgotPassword = ({isAlreadyLoggedIn,forgotPassword}) => {
+
+    if (isAlreadyLoggedIn === true) {
+        return <Redirect to="/home" />
+    }
 
     return  (
         <Row type="flex" align="middle" justify="center" style={{height:"100vh"}}>
             
             <Col xs={20} lg={6}>
-                <ForgotPasswordForm />
+                <ForgotPasswordForm onSubmit={forgotPassword} />
             </Col>
         
         </Row>
     );
 
 };
+
+const mapStateToProps = state => ({
+    isAlreadyLoggedIn: state.authReducer.loggedIn
+})
+
+export default connect(mapStateToProps,{forgotPassword})(ForgotPassword)
