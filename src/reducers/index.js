e30c2e1f607fa4eb26c990ef99849051e98ac7d6
vitalii <vitalii.vorobij@gmail.com>
2020-02-13 17:35:19 +0200

    Combined added reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 import {combineReducers} from 'redux'
 import auth from './authReducer'
+import alert from './alertReducer'
+import loading from './loadingReducer'
+import accountVerification from './accountVerificationReducer'
 
 export default combineReducers({
-    authReducer: auth
+    authReducer: auth,
+    alertReducer: alert,
+    loadingReducer: loading,
+    accountVerificationReducer: accountVerification
 })
