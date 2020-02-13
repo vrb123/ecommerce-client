a75999ca98c25e16d6b33cd2eeb1c7add892c396
vitalii <vitalii.vorobij@gmail.com>
2020-02-13 17:35:00 +0200

    Changed Auth Reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,15 +1,10 @@
-import {USER_LOGIN_FAILURE,USER_LOGIN_SUCCESS,USER_LOGIN_REQUEST,USER_LOGOUT} from '../actions/types';
+import {USER_LOGIN_SUCCESS,USER_LOGOUT,USER_LOGIN_FAILURE} from '../actions/types';
 
 const user = JSON.parse(localStorage.getItem('user'))
 const initialState = user ? { loggedIn: true, user } : {}
 
 export default (state = initialState, action ) => {
     switch(action.type) {
-        case USER_LOGIN_REQUEST:
-            return {
-                logginIn: true,
-                user: action.payload
-            }
         case USER_LOGIN_SUCCESS:
             return {
                 loggedIn: true,
