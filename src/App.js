f7a13540390771cf7e47153a3b395b9b6c78fd5e
vitalii <vitalii.vorobij@gmail.com>
2020-02-13 17:39:21 +0200

    Provided routing

--- src/App.js
+++ src/App.js
@@ -9,24 +9,34 @@ import {Route,Switch} from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import ForgotPassword from './components/ForgotPassword';
+import Home from './components/Home';
+import ResetPassword from './components/ResetPassword';
+import VerifyAccount from './components/VerifyAccount';
 
+// Private route
 import {PrivateRoute} from './components/PrivateRoute'
 
 
 // Redux
 import {Provider} from 'react-redux';
 import store from './store'
-import Home from './components/Home';
+
+// Alert
+import Alert from './components/Alert';
+
 
 export default () => {
     return (
         <Provider store={store}>
+            <Alert/>
             <Layout>
                 <Switch>
                     <Route exact path='/' component={Login} />
                     <Route path="/login" component={Login} />
                     <Route path="/register" component={Register} />
+                    <Route path="/verify/:token" component={VerifyAccount} />
                     <Route path="/forgotPassword" component={ForgotPassword} />
+                    <Route path="/resetPassword/:token" component={ResetPassword} />
                     <PrivateRoute path="/home" component={Home} />
                 </Switch>
             </Layout>
