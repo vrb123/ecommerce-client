86383e0c63961b03aa146422d2e5fd9fb4a10d8c
vitalii <vitalii.vorobij@gmail.com>
2020-02-13 12:25:49 +0200

    Added Header component

---
+++ src/layout/Header/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import "./index.css";
+
+const { Header } = Layout;
+
+export default () => {
+
+    return (
+        <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+            <div className="logo" />
+            <Menu
+                theme="dark"
+                mode="horizontal"
+               // defaultSelectedKeys={['2']}
+                style={{ lineHeight: '64px' }}
+            >
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+    </Header>
+    )
+};
