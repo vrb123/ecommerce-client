30ee260e0f45578a621e24453903094866fcb59c
vitalii <vitalii.vorobij@gmail.com>
2020-02-17 11:47:58 +0200

    Changed App.js, maintained routing.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Layout } from 'antd';
 
+import "./App.css";
+
 // Routing
 import {Route,Switch} from 'react-router-dom';
 
@@ -16,30 +18,40 @@ import VerifyAccount from './components/VerifyAccount';
 // Private route
 import {PrivateRoute} from './components/PrivateRoute'
 
-
 // Redux
 import {Provider} from 'react-redux';
 import store from './store'
 
 // Alert
 import Alert from './components/Alert';
+import { AdminRoute } from './components/AdminRoute';
+import Admin from './components/Admin';
+
+// Jwt Listener
+import JwtListener from './components/JwtListener';
 
 
 export default () => {
     return (
         <Provider store={store}>
-            <Alert/>
-            <Layout>
-                <Switch>
-                    <Route exact path='/' component={Login} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/register" component={Register} />
-                    <Route path="/verify/:token" component={VerifyAccount} />
-                    <Route path="/forgotPassword" component={ForgotPassword} />
-                    <Route path="/resetPassword/:token" component={ResetPassword} />
-                    <PrivateRoute path="/home" component={Home} />
-                </Switch>
-            </Layout>
+            <JwtListener>
+                <Alert/>
+                <Layout>
+                    <Switch>
+                        <Route exact path='/' component={Login} />
+
+                        <Route path="/login" component={Login} />
+                        <Route path="/register" component={Register} />
+                        <Route path="/verify/:token" component={VerifyAccount} />
+                        <Route path="/forgotPassword" component={ForgotPassword} />
+                        <Route path="/resetPassword/:token" component={ResetPassword} />
+                        
+                        <PrivateRoute path="/home" component={Home} />
+                        
+                        <AdminRoute path="/admin" component={Admin} />
+                    </Switch>
+                </Layout>
+            </JwtListener>
         </Provider>
     )
 };
