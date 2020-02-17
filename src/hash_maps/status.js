7f6ed299930c4fb88df560cabef9c926f429a58b
vitalii <vitalii.vorobij@gmail.com>
2020-02-17 11:40:01 +0200

    Added hash maps to prettify UI

---
+++ src/hash_maps/status.js
@@ -0,0 +1,7 @@
+import {ACTIVE,NOT_ACTIVE,DELETED} from '../constants/user_status'
+
+export default ({
+    [ACTIVE]: 'Active',
+    [NOT_ACTIVE]: 'Not active',
+    [DELETED]: 'Deleted'
+})
---
+++ src/hash_maps/user_roles.js
@@ -0,0 +1,6 @@
+import {ADMIN,CUSTOMER} from '../constants/user_roles'
+
+export default ({
+    [ADMIN]:'Admin',
+    [CUSTOMER]:'Customer'
+})
