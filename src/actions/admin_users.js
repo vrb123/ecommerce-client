e1d965e04bcce7d9512f448bbfd20abbd74d0f49
vitalii <vitalii.vorobij@gmail.com>
2020-02-17 11:40:48 +0200

    Added Admin actions regarding to users

---
+++ src/actions/admin_users.js
@@ -0,0 +1,230 @@
+import {
+    REQUEST_SEND,
+    ALERT_SUCCESS,
+    ALERT_FAILURE,
+    JWT_TOKEN_WRONG,
+    ADMIN_GET_ALL_USERS,
+    ADMIN_EDIT_USER,
+    ADMIN_DELETE_USER,
+    ADMIN_GET_USER_BY_ID,
+    ALERT_INFO
+}
+from './types';
+import config from '../config';
+import assertResponseOK from './assertResponseOk'
+import assertNotForbidden from './assertNotForbidden'
+import authHeader from '../helpers/auth_header'
+import ForbiddenError from '../exceptions/ForbiddenError'
+
+export const getUsers = () => async dispatch => {
+    
+    dispatch({
+        type: REQUEST_SEND
+    })
+
+    try {
+        const response = await fetch(`${config.API_URL}/admin/users`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                ...authHeader()
+            },
+        });
+
+        assertNotForbidden(response)
+        assertResponseOK(response)
+
+        const users = await response.json()
+
+        console.log(users);
+
+        setTimeout( () => {
+            
+            dispatch({
+                type: ALERT_SUCCESS,
+                payload: 'Successfully got users'
+            })
+
+            dispatch({
+                type: ADMIN_GET_ALL_USERS,
+                payload: users
+            })
+
+        },1000 )
+        
+    }
+    catch(err) {
+
+        if(err.name === 'ForbiddenError') {
+            dispatch({
+                type: JWT_TOKEN_WRONG
+            })
+            return;
+        }
+
+        dispatch({
+            type: ALERT_FAILURE,
+            payload: 'Failed to fetch users'
+        })
+    } 
+};
+
+
+export const getUserById = id => async dispatch => {
+    
+    dispatch({
+        type: REQUEST_SEND
+    })
+
+    try {
+        const response = await fetch(`${config.API_URL}/admin/users/${id}`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                ...authHeader()
+            },
+        });
+
+        assertNotForbidden(response)
+        assertResponseOK(response)
+
+        const user = await response.json()
+
+        setTimeout( () => {
+            
+            dispatch({
+                type: ALERT_SUCCESS,
+                payload: 'Successfully got user'
+            })
+
+            dispatch({
+                type: ADMIN_GET_USER_BY_ID,
+                payload: user
+            })
+
+        },1000 )
+        
+    }
+    catch(err) {
+
+        console.log(err.name);
+
+        if(err.name === 'ForbiddenError') {
+            dispatch({
+                type: JWT_TOKEN_WRONG
+            })
+            return;
+        }
+
+        dispatch({
+            type: ALERT_FAILURE,
+            payload: 'Failed to fetch user'
+        })
+    } 
+};
+
+
+export const deleteUser = id => async dispatch => {
+    
+    dispatch({
+        type: REQUEST_SEND
+    })
+
+    try {
+        const response = await fetch(`${config.API_URL}/admin/users/${id}`,{
+            method: 'POST',
+            'mode': 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                ...authHeader(),
+            },
+        });
+
+        assertNotForbidden(response)
+        assertResponseOK(response)
+
+        setTimeout( () => {
+            
+            dispatch({
+                type: ALERT_INFO,
+                payload: 'Successfully deleted user'
+            })
+
+            dispatch({
+                type: ADMIN_DELETE_USER,
+                payload: id
+            })
+
+        },1000 )
+        
+    }
+    catch(err) {
+
+        if(err.name === 'ForbiddenError') {
+            dispatch({
+                type: JWT_TOKEN_WRONG
+            })
+            return;
+        }
+
+        dispatch({
+            type: ALERT_FAILURE,
+            payload: 'Registration failed'
+        })
+    } 
+};
+
+
+
+export const editUser = (id,data) => async dispatch => {
+    
+    dispatch({
+        type: REQUEST_SEND
+    })
+
+    try {
+        const response = await fetch(`${config.API_URL}/admin/users/edit/${id}`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                ...authHeader()
+            },
+            body: JSON.stringify(data)
+        });
+
+        assertNotForbidden(response)
+        assertResponseOK(response)
+
+        setTimeout( () => {
+            
+            dispatch({
+                type: ALERT_INFO,
+                payload: 'Successfully edited user'
+            })
+
+            dispatch({
+                type: ADMIN_EDIT_USER,
+                payload: {
+                    id,
+                    ...data
+                }
+            })
+
+        },1000 )
+        
+    }
+    catch(err) {
+       
+        if(err.name === 'ForbiddenError') {
+            dispatch({
+                type: JWT_TOKEN_WRONG
+            })
+            return;
+        }
+
+        dispatch({
+            type: ALERT_FAILURE,
+            payload: 'Failed to edit user'
+        })
+    } 
+};
