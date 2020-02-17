b33ac4c469398cf34a36dce607fb0f73af05de90
vitalii <vitalii.vorobij@gmail.com>
2020-02-17 11:44:57 +0200

    Combined mentioned before reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,14 @@ import auth from './authReducer'
 import alert from './alertReducer'
 import loading from './loadingReducer'
 import accountVerification from './accountVerificationReducer'
+import adminUsers from './adminUsersReducer'
+import jwt from './jwtReducer'
 
 export default combineReducers({
     authReducer: auth,
     alertReducer: alert,
     loadingReducer: loading,
-    accountVerificationReducer: accountVerification
+    accountVerificationReducer: accountVerification,
+    adminUsersReducer: adminUsers,
+    jwtReducer: jwt
 })
