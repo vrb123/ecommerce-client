c6cb30d4f06cfbb29729ba6f35064bf6a8e4335d
vitalii <vitalii.vorobij@gmail.com>
2020-02-17 11:45:27 +0200

    Added RoleSelect component

---
+++ src/components/Admin/AdminUsers/UserEdit/UserEditForm/RoleSelect/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Select} from 'antd';
+
+import userRolesMap from '../../../../../../hash_maps/user_roles';
+
+export default ({onChange,options,selected}) => {
+
+    const filteredOptions = options.filter( o => !selected.includes(o) );
+    
+    return (
+        <Select
+            mode="multiple"
+            placeholder="Select roles"
+           labelInValue
+            value={selected}
+            onChange={ newSelected => {
+                onChange(newSelected)
+            }  }
+            style={{width:'100%'}}
+        >
+            {
+                filteredOptions.map((option,i) => (
+                    
+                    <Select.Option key={i} value={option} label={userRolesMap[option]}>
+                        {userRolesMap[option]}
+                    </Select.Option>
+                
+                ))
+            }
+        </Select>
+    );
+};
